1f5f30c6279c51c04258b7116ec95072863c4abb
elomek <elhamkramian9@gmail.comgit config --global user.email elhamkaramian90@gmail.comgit config --global user.email elhamkramian9@gmail.com>
2024-06-25 11:42:24 +0200

    panel admin

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'id', 'author', 'status']
+
+
+admin.site.register(Post,PostAdmin)
+
